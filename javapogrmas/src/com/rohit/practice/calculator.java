84fd09475ccfafd68f7a78bfa8b82fd5e0783be1
Rohit jha <kumarrohi88@gmail.com>
2023-11-23 16:24:46 +0530

    d

---
+++ javapogrmas/src/com/rohit/practice/calculator.java
@@ -0,0 +1,15 @@
+package com.rohit.practice;
+
+public class calculator {
+	int a=50;
+	int b=60;
+void add() {
+	int c=a+b;
+	System.out.println(c);
+	
+}
+public static void main(String args[]) {
+	calculator calc =new calculator();
+	calc.add();
+}
+}
